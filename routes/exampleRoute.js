53681b9043ea10f05792a4f3adfe9919b89eb862
tauseef-creator <i211236@nu.edu.pk>
2024-10-29 00:19:32 +0500

    fix import error

--- routes/exampleRoute.js
+++ routes/exampleRoute.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getAllUsers, createUser, getUserById } from '../controllers/exampleController';
+import { getAllUsers, createUser, getUserById } from '../controllers/exampleController.js';
 
 const router = express.Router();
 
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
-import exampleRouter from './routes/exampleRoute';
+import exampleRouter from './routes/exampleRoute.js';
 
 dotenv.config();
 
